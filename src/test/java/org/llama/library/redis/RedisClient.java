dd24868e8dbb22f546a7a4d1c7016f14fc66ca86
tonny1228 <tonny1228@163.com>
2016-06-08 17:25:23 +0800

    test redis

---
+++ src/test/java/org/llama/library/redis/RedisClient.java
@@ -0,0 +1,46 @@
+package org.llama.library.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+/**
+ * Created by Tonny on 2016/6/8.
+ */
+public class RedisClient {
+    private Jedis jedis;//非切片额客户端连接
+    private JedisPool jedisPool;//非切片连接池
+    private ShardedJedis shardedJedis;//切片额客户端连接
+    private ShardedJedisPool shardedJedisPool;//切片连接池
+
+    public RedisClient() {
+        initialPool();
+        initialShardedPool();
+        shardedJedis = shardedJedisPool.getResource();
+        jedis = jedisPool.getResource();
+
+
+    }
+
+    /**
+     * 初始化非切片池
+     */
+    private void initialPool() {
+        // 池基本配置
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(20);
+        config.setMaxIdle(5);
+        config.setMaxWaitMillis(1000l);
+        config.setTestOnBorrow(true);
+
+        jedisPool = new JedisPool(config, "127.0.0.1", 6379);
+    }
+
+
+    public void show() {
+       
+    }
+
+}
